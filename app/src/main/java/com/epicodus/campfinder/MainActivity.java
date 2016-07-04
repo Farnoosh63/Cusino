7cdfc9466be502fe3433d521f235e78fd673a2b2
Farnoosh <farnoosh@pdx.edu>
2016-07-04 15:24:48 -0700

    mainActivity functionality

---
+++ app/src/main/java/com/epicodus/campfinder/MainActivity.java
@@ -0,0 +1,45 @@
+package com.epicodus.campfinder;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Bind(R.id.radioButtonYes) RadioButton mRadioButtonYes;
+    @Bind(R.id.radioButtonNo) RadioButton mRadioButtonNo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        mRadioButtonYes.setOnClickListener(this);
+        mRadioButtonNo.setOnClickListener(this);
+    }
+
+
+
+
+    @Override
+    public void onClick(View v) {
+        if (v == mRadioButtonYes) {
+            Intent intent = new Intent(MainActivity.this, AccountActivity.class);
+            startActivity(intent);
+            } else {
+                Intent intent = new Intent(MainActivity.this, AccountActivity.class);
+                startActivity(intent);
+        }
+    }
+
+}
+
+
