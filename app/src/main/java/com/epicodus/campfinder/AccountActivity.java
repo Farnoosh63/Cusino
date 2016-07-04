daf2ccb5f82242d03bfd928f0989f05c4dcedbe6
Farnoosh <farnoosh@pdx.edu>
2016-07-04 16:03:33 -0700

    add user activity

--- app/src/main/java/com/epicodus/campfinder/AccountActivity.java
+++ app/src/main/java/com/epicodus/campfinder/AccountActivity.java
@@ -1,13 +1,40 @@
 package com.epicodus.campfinder;
 
+import android.accounts.Account;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class AccountActivity extends AppCompatActivity {
+    @Bind(R.id.name) EditText mName;
+    @Bind(R.id.password) EditText mPassword;
+    @Bind(R.id.email) EditText mEmail;
+    @Bind(R.id.submitbutton)
+    Button mSubmitButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        ButterKnife.bind(this);
+
+    mSubmitButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String name = mName.getText().toString();
+            String password = mPassword.getText().toString();
+            String email = mEmail.getText().toString();
+            Intent intent = new Intent(AccountActivity.this, UserActivity.class);
+            startActivity(intent);
+        }
+     });
     }
 }
